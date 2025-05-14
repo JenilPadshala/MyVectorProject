6272c175ed7bb93ee7b24b4afddbdd3b2616a924
Jenil Padshala <jenilpadshala@gmail.com>
2025-05-14 22:53:17 +0530

    Initial MyVector class shell and main.cpp structure

---
+++ MyVector.hpp
@@ -0,0 +1,17 @@
+#ifndef MYVECTOR_HPP
+#define MYVECTOR_HPP
+
+#include <cstddef>
+
+template <typename T>
+class MyVector {
+private:
+    T* m_data;              // Pointer to the array of elements
+    size_t m_size;          // Number of elements in the vector
+    size_t m_capacity;      // Current allocated storage capacity
+
+public:
+
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "MyVector.hpp"
+
+int main(){
+    std::cout << "MyVector Project: Testing started." << std::endl;
+
+    std::cout << "MyVector Project: Testing finished." << std::endl;
+    return 0;
+}
